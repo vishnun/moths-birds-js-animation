d40682eb5263041036e939f3fdd65106672cc664
Vishnu <vishnunarang2000@gmail.com>
2018-01-16 10:25:31 -0700

    Adding new images for after IR view.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function(argument) {
 
   $('#before-IR-btn').on('click', function() {
+    $('body').removeClass('after-ir');
+    $('body').addClass('before-ir');
+
     var beforeIndustrialRevolution = true;
     var mothApp = new MothApp(beforeIndustrialRevolution);
     mothApp.init();
@@ -9,6 +12,9 @@ $(function(argument) {
   });
 
   $('#after-IR-btn').on('click', function() {
+    $('body').addClass('after-ir');
+    $('body').removeClass('before-ir');
+
     var beforeIndustrialRevolution = false;
     var mothApp = new MothApp(beforeIndustrialRevolution);
     mothApp.init();
